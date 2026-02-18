83f389e85cd85cfbc845440435d075bf742cf7f0
kiloexabyte <vothanhdat278+1@gmail.com>
2026-02-17 22:04:44 -0600

    Add prettier with tailwind plugin and format code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-    <h1 class="text-4xl font-bold">Web games or watever</h1>
+	<h1 class="text-4xl font-bold">Web games or watever</h1>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      '@tailwindcss/postcss': {}
-    }
-  }
-})
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	css: ["~/assets/css/main.css"],
+	postcss: {
+		plugins: {
+			"@tailwindcss/postcss": {},
+		},
+	},
+});
